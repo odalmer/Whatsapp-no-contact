7289884568a6d6934f4df09c122f6bcf56fb2fd3
odalmer <48610363+odalmer@users.noreply.github.com>
2022-01-02 19:50:46 -0800

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-var numInput = document.getElementById("numInput");
-var submitBtn = document.getElementById("submitBtn");
-
-submitBtn.addEventListener("click", function () {
-  var url = "https://api.whatsapp.com/send?phone=" + numInput.value;
-  window.location.replace(url);
-  alert("successful operation");
-});
